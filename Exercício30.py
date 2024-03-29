e138a0c8e53bd136c3fad0accc984cdb98592ce7
Paulo J. Veroneze <44308731+TioKyoto@users.noreply.github.com>
2024-03-28 22:16:52 -0300

    Exercício30.py

--- Exercício30.py
+++ Exercício30.py
@@ -1,6 +1,6 @@
 vlrhr = float(input("informe o quanto você recebe por hora: "))
 hr = float(input("informe quantas horas você trabalhou no mês: "))
 totalhr = vlrhr * hr
-acrecimo = totalhr * 0.10
+acrecimo = totalhr * 0.14
 totalacre = totalhr + acrecimo
 print("seu salario no final do mês foi: ", totalacre)
